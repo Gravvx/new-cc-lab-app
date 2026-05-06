a63f42733ca3659930f2357814c2cba39291474c
Gravvx <forstudywork24x7@gmail.com>
2026-05-07 02:03:51 +0530

    Remove extra print statement from program.py

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-print("Hello from Google App Engine! This is my Cloud Computing Lab.").
+print("Hello from Google App Engine! This is my Cloud Computing Lab.")
 print("Hello from Google App Engine! This is my Cloud Computing Lab.")
 print("Hello from Google App Engine! This is my Cloud Computing Lab.")
 print("Hello from Google App Engine! This is my Cloud Computing Lab.")
