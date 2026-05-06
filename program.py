f9af2fc0c711be77d4a5f9a45d7a8a8202a693a6
Gravvx <forstudywork24x7@gmail.com>
2026-05-07 01:56:36 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,7 @@
+print("Hello from Google App Engine! This is my Cloud Computing Lab.").
+print("Hello from Google App Engine! This is my Cloud Computing Lab.")
+print("Hello from Google App Engine! This is my Cloud Computing Lab.")
+print("Hello from Google App Engine! This is my Cloud Computing Lab.")
+print("Hello from Google App Engine! This is my Cloud Computing Lab.")
+print("Hello from Google App Engine! This is my Cloud Computing Lab.")
+print("Hello from Google App Engine! This is my Cloud Computing Lab.")
